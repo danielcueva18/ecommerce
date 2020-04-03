502149c41b12aeeec630f78837118f3b1e82e5b0
Daniel Cueva <cuevadaniel1@gmail.com>
2020-04-02 21:01:57 -0700

    ability to create, update, delete, get, getoneby and write all users to our json file

---
+++ repositories/users.js
@@ -0,0 +1,103 @@
+const fs = require('fs')
+const crypto = require('crypto')
+
+class UsersRepository {
+    constructor(filename) {
+        if (!filename) {
+            throw new Error('Creating a repository requires a filename')
+        }
+
+        this.filename = filename;
+        try {
+            fs.accessSync(this.filename);
+        } catch (err) {
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+
+    async getAll() {
+        return JSON.parse(
+            await fs.promises.readFile(this.filename, {
+            encoding: 'utf8'
+        })
+    );
+}
+
+
+// Create "create" function to add new user info
+    async create(attrs) {
+        attrs.id = this.randomId();
+
+        const records = await this.getAll();
+        records.push(attrs);
+        await this.writeaAll(records);
+    }
+
+    async writeaAll(records) {
+        await fs.promises.writeFile(
+            this.filename,
+            JSON.stringify(records, null, 2)
+        );
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex'); // create random id
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+        return records.find(records => records.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+        await this.writeaAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id)
+
+        if(!record) {
+            throw new Error(`Record with id ${id} not found`)
+        }
+        //record == { email: 'test@test.com }
+        // record === { password: 'mypassword' }
+        Object.assign(record, attrs);
+        // record === { email: 'test@test.com, password: 'myapassword'}
+        await this.writeaAll(records)
+    }
+
+    async getOneBy (filters) {
+        const records = await this.getAll();
+
+        for (let record of records) { // of - iterating through arrays
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+
+            if (found) {
+                return record;
+            }
+        }
+    }
+};
+const test = async () => {
+    const repo = new UsersRepository('users.json'); 
+
+    const user = await repo.getOneBy({
+        email: 'test@test.com',
+        password: 'password',
+        id: 'a7c04b42'
+    })
+
+    console.log(user);
+}
+
+
+test();
