b169ec9346399e54304baf8c6acdecd78a5f38b3
Daniel Cueva <cuevadaniel1@gmail.com>
2020-04-04 18:09:18 -0700

    add ability to sign out

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     keys: ['asdfjlaskdjflasldkj98798a789']
 }));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
             Your id is: ${req.session.userId}
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, passwordConfirmation} = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -45,6 +45,43 @@ app.post('/', async (req, res) => {
 
     res.send('Account created!')
 });
+
+app.get('/signout', (req, res) => {
+    // forget all information inside the cookie
+    req.session = null;
+
+    res.send('You are signed out')
+})
+
+app.get('/signin', (req, res)=> {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+        return res.send('Email not found')
+    }
+
+    if (user.password != password) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!')
+})
 
 app.listen(3000, () => {
     console.log('Listening..');
