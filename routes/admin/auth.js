75c080ea7641ea5dc364d180ce7ec762f650da16
Daniel Cueva <cuevadaniel1@gmail.com>
2020-05-04 22:55:16 -0700

    reusable error handling middleware

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
 
+const { handleErrors} = require('./middlewares')
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
@@ -21,15 +21,10 @@ router.get('/signup', (req, res) => {
 
 router.post(
     '/signup', 
-    [requireEmail, requirePassword, requirePasswordConfirmation], 
+    [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
 async (req, res) => {
-    const errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.send(signupTemplate( { req, errors }));
-    }
-
-    const { email, password, passwordConfirmation} = req.body;
+    const { email, password } = req.body;
 
     if (password !== passwordConfirmation) {
         return res.send('Passwords must match')
@@ -52,14 +47,10 @@ router.get('/signin', (req, res) => {
 });
 
 router.post('/signin',
-[requireEmailExist, requireValidPasswordForUser], 
+[requireEmailExist, requireValidPasswordForUser],
+handleErrors(signinTemplate),
 async (req, res) => {
-   const errors = validationResult(req);
-   
-   if (!errors.isEmpty()) {
-       return res.send(signinTemplate( { errors }));
-   }
-   
+
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
