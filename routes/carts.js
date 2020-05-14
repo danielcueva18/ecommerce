fb3f28b7e3ae26c9bd4770bc4d392d5a63c3b3c3
Daniel Cueva <cuevadaniel1@gmail.com>
2020-05-13 18:16:50 -0700

    submissions options

--- routes/carts.js
+++ routes/carts.js
@@ -2,4 +2,11 @@ const express = require ('express');
 
 const router = express.Router();
 
+// receive a post request to add an item to a cart
+router.post('/cart/products', (req, res) => {
+    console.log(req.body.productId);
+
+    res.send('Product added to cart');
+})
+
 module.exports = router;
