27f31b95a79f72d1873393fff4cf5abc8422f103
Daniel Cueva <cuevadaniel1@gmail.com>
2020-05-13 18:55:08 -0700

    creating a cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,22 @@
 const express = require ('express');
+const cartsRepo = require('../repositories/carts')
 
 const router = express.Router();
 
 // receive a post request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-    console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+    // figure out the cart
+    let cart;
+    if (!req.session.cartId) {
+        // We don't have a cart, we need to create one
+        // and store the cart id on the req.session
+        cart = await cartsRepo.create({items: []})
+        req.session.cartId = cart.id;
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId)
+    }
+
+    console.log(cart)
 
     res.send('Product added to cart');
 })
