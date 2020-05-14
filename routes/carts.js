7b53b65dbb9481196ef2ad9a3f2703c7ccbf9476
Daniel Cueva <cuevadaniel1@gmail.com>
2020-05-13 19:10:31 -0700

    adding items to the cart

--- routes/carts.js
+++ routes/carts.js
@@ -16,8 +16,16 @@ router.post('/cart/products', async (req, res) => {
         cart = await cartsRepo.getOne(req.session.cartId)
     }
 
-    console.log(cart)
-
+    const existingItem = cart.items.find(item => item.id === req.body.productId)
+    if (existingItem) {
+        // increment and save on cart
+        existingItem.quantity++;
+    } else {
+        cart.items.push({ id: req.body.productId, quantity: 1})
+    }
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
     res.send('Product added to cart');
 })
 
